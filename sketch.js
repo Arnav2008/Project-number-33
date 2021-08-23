0089e2f03df4baf1587f4dba1f782189d43802ae
Arnav Singh <76939354+Arnav2008@users.noreply.github.com>
2021-08-23 12:51:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+ 
+
+
+
+var engine,world;
+var backgroundImg;
+var particles=[];
+
+
+
+function setup() {
+  
+  
+    createCanvas(800,400);
+  
+}
+
+function preload(){
+  backgroundImg=loadImage("snow2.jpg");
+  
+}
+
+
+                        
+
+
+function draw() {
+  background(backgroundImg);  
+  drawSprites();
+
+
+  if(frameCount%60===0) {
+    particles.push(new Particles(random(0,800),0))
+    
+    
+    }
+
+    for (var k = 0; k < particles.length; k++) {
+      particles[k].display();
+    }
+  
+
+  
+
+}
